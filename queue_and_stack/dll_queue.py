fa993b7d5055ebfeb04a7269ce47ec9186e43d41
vpagano10 <vpagano10@gmail.com>
2020-04-20 18:35:51 -0400

    Done with stack and queue mvp.

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,28 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
+# FIFO
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # The list will be changing constantly, and in addition is is faster to add/remove items from the beginning of a linked list than an array because the elements do not need to "shift". We also do not need to lookup specific items, so the benefit of the indexing of an array does not help here.
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # length += 1
+        self.size = + 1
+        # add an item to the end of the list, add_to_tail
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        # if no head node, return none
+        if not self.storage.head:
+            return None
+        else:
+            # decrease by 1, by removing head
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,28 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
+# FILO
 class Stack:
-    def __init__(self):
+    def __init__(self, node=None):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # When the data set is changing very often, it makes more sense to use a linked list over an array because it is faster for the elements to be removed or deleted from the linked list. We also do not need to look for specific elements, we just need the last one in this case.
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # length += 1
+        self.size += 1
+        # add node to tail
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        # if no tail just return
+        if not self.storage.tail:
+            return None
+        # if there is a tail, remove it
+        else:
+            self.size -= 1
+            return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
